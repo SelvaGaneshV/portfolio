d3fc66667eb6456ca44aa63f878ae680d5b04b06
SelvaGaneshV <v.a.ganesh8@gmail.com>
2026-01-26 12:02:39 +0530

    updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { resolve } from "node:path";
 import { nitro } from "nitro/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    devtools({
+      removeDevtoolsOnBuild: true,
+    }),
     nitro(),
     tanstackStart(),
     viteReact({
