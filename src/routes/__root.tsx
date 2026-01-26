04f37a30a03e89ed822bafc9f34d281398096766
SelvaGaneshV <v.a.ganesh8@gmail.com>
2026-01-26 14:20:06 +0530

    updated title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,7 +14,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "Ganork",
+        title: "Selva Ganesh",
       },
     ],
     links: [
