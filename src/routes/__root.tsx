4cf580dddad9d697856eda026a8273a1157c371e
SelvaGaneshV <ganeshv@torus.tech>
2026-03-20 11:22:35 +0530

    added preconnect

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,9 +93,15 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+      },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Inter:wght@500&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Quicksand:wght@300..700&family=Roboto+Mono:ital,wght@1,100..700&family=Slackside+One&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Inter:wght@500&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Quicksand:wght@300..700&family=Roboto+Mono:ital,wght@1,100..700&family=Slackside+One&display=optional",
       },
       {
         rel: "icon",
