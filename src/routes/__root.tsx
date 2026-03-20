0e61230431f4a9800bb72593e540f589fac52bb1
SelvaGaneshV <ganeshv@torus.tech>
2026-03-20 10:57:56 +0530

    Revert "add font preconnect"
    This reverts commit cbdc3a31bba6f3a9dfe7a38733fd3d7abec98a94.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -75,7 +75,7 @@ export const Route = createRootRoute({
       },
       {
         name: "twitter:title",
-        content: "Selva Ganesh V - Full Stack Developer",
+        content: "Selva Ganesh - Full Stack Developer",
       },
       {
         name: "twitter:description",
@@ -92,16 +92,10 @@ export const Route = createRootRoute({
       },
     ],
     links: [
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossOrigin: "anonymous",
-      },
       { rel: "stylesheet", href: appCss },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Inter:wght@500&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Quicksand:wght@300..700&family=Roboto+Mono:ital,wght@1,100..700&family=Slackside+One&display=swap",
       },
       {
         rel: "icon",
